c973a90e400f557d91770150daa96254f07948c6
jmbelmonte <jmbelmonte11@gmail.com>
2020-06-28 12:11:48 +0800

    JS Test 3
    JS Test 3

---
+++ script.js
@@ -0,0 +1,8 @@
+var username = prompt("Username?");
+var password = prompt("Password?");
+
+if (username === "jm" && password === "123") {
+    alert("Welcome to the system!");
+} else {
+    alert("Wrong username and password!");
+}
