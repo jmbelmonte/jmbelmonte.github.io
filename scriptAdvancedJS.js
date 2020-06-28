4edbd989b5dd35234b05b70499fda45156a8a94d
jmbelmonte <jmbelmonte11@gmail.com>
2020-06-28 22:43:53 +0800

    Upd

--- scriptAdvancedJS.js
+++ scriptAdvancedJS.js
@@ -38,7 +38,7 @@ console.log(compose(add1, add5)(10)) */
 
 // Advanced Array
 
-const array = [1, 2, 3, 4];
+/* const array = [1, 2, 3, 4];
 
 const double = [];
 
@@ -60,4 +60,314 @@ const reduceArray = array.reduce((acc,num) => {
     return acc + num; //1 3 6 10
 }, 0);
 
-console.log('reduceArray', reduceArray);
+console.log('reduceArray', reduceArray); */
+
+// Complete the below questions using this array:
+/* const array = [
+	{
+		username: "john",
+		team: "red",
+		score: 5,
+		items: ["ball", "book", "pen"]
+	},
+	{
+		username: "becky",
+		team: "blue",
+		score: 10,
+		items: ["tape", "backpack", "pen"]
+	},
+	{
+		username: "susy",
+		team: "red",
+		score: 55,
+		items: ["ball", "eraser", "pen"]
+	},
+	{
+		username: "tyson",
+		team: "green",
+		score: 1,
+		items: ["book", "pen"]
+	},
+
+];
+
+//Create an array using forEach that has all the usernames with a "!" to each of the usernames
+let newArray = []
+array.forEach(user => {
+	let { username } = user;
+	username = username + "!";
+	newArray.push(username);
+})
+console.log(newArray);
+
+//Create an array using map that has all the usernames with a "?" at the end of usernames
+const mapArray = array.map(user => {
+	let { username } = user;
+	return username + "?";
+})
+console.log(mapArray);
+
+//Filter the array to only include users who are on team: red
+const filterArray = array.filter(user => {
+	return user.team === "red";
+})
+console.log(filterArray);
+
+//Find out the total score of all users using reduce
+const total = array.reduce((acc, user) => {
+	return acc + user.score
+}, 0);
+console.log(total);
+
+
+// (1), what is the value of i? INDEX of the array.
+// (2), Make this map function pure:
+const arrayNum = [1, 2, 4, 5, 8, 9];
+const newArray2 = arrayNum.map((num, i) => {
+	return num * 2;
+})
+
+
+
+//BONUS: create a new list with all user information, but add "!" to the end of each items they own.
+const answer = array.map(user => {
+	user.items = user.items.map(item => {
+		return item + "!"
+	});
+	return user;
+})
+console.log('BONUS', answer); */
+
+
+
+
+//Evaluate these:
+//#1
+//[2] === [2] // false
+//{} === {} // false
+
+//#2 what is the value of property a for each object.
+/* const object1 = { a: 5 }; // 4
+const object2 = object1; // 4
+const object3 = object2; // 4
+const object4 = { a: 5}; // 5
+object1.a = 4;
+
+
+//#3 create two classes: an Animal class and a Mamal class. 
+// create a cow that accepts a name, type and color and has a sound method that moo's her name, type and color. 
+class Animal {
+	constructor(name, type, color) {
+		this.name = name;
+		this.color = color;
+		this.type = type;
+	}
+}
+
+class Mamal extends Animal {
+	constructor(name, type, color) {
+		super(name, type, color)
+	}
+	sound() {
+		console.log(`Moooo I'm ${this.name} and I'm a ${this.color} ${this.type}`);
+	}
+}
+
+const cow = new Mamal('Shelly', 'cow', 'brown');
+
+console.log(cow);
+
+cow.sound(); */
+
+// Solve the below problems:
+
+// #1) Check if this array includes the name "John".
+/* const dragons = ['Tim', 'Johnathan', 'Sandy', 'Sarah'];
+
+dragons.includes('John') // false
+
+// #2) Check if this array includes any name that has "John" inside of it. If it does, return that
+// name or names in an array.
+const dragons2 = ['Tim', 'Johnathan', 'Sandy', 'Sarah'];
+
+dragons2.filter(name => name.includes('John')) // ['Johnathan']
+
+// #3) Create a function that calulates the power of 100 of a number entered as a parameter
+const power100 = (num) => num**100;
+
+// #4) Useing your function from #3, put in the paramter 10000. What is the result?
+// Research for yourself why you get this result.
+
+power100(10000); // Infinity */
+
+// Solve the below problems:
+
+// #1) Line up the Turtle and the Rabbit at the start line:
+/* const startLine = '     ||<- Start line';
+let turtle = 'ðŸ¢';
+let rabbit = 'ðŸ‡';
+
+// it should look like this:
+'     ||<- Start line'
+'       ðŸ¢'
+'       ðŸ‡'
+
+// when you do:
+console.log(startLine);
+console.log(turtle);
+console.log(rabbit);
+
+
+turtle = turtle.padStart(8);
+rabbit = rabbit.padStart(8);
+
+
+// #2) What happens when you run turtle.trim().padEnd(9, '=') on the turtle variable
+// Read about what the second parameter does in padEnd and padStart
+turtle = turtle.trim().padEnd(9, '=');
+
+'     ||<- Start line'
+'ðŸ¢======='
+'       ðŸ‡'
+
+
+// #3) Get the below object to go from:
+let obj = {
+  my: 'name',
+  is: 'Rudolf',
+  the: 'raindeer'
+}
+// to this:
+'my name is Rudolf the raindeer'
+
+Object.entries(obj).map(value => value.join(" ")).join(' ') */
+
+// Solve the below questions:
+
+// #1 Turn this array into a new array: [1,2,3,[4],[5]]. Bonus if you can do it on one line
+/* const array = [[1],[2],[3],[[[4]]],[[[5]]]]
+//Solution:
+console.log(array.flat(2))
+
+
+// #2 Turn this array into a new array: [ 'Hello young grasshopper!', 'you are', 'learning fast!' ]
+const greeting = [["Hello", "young", "grasshopper!"], ["you", "are"], ["learning", "fast!"]];
+//Solution:
+console.log(greeting.flatMap(x => x.join(' ')))
+
+
+//#3 Turn the greeting array above into a string: 'Hello young grasshopper you are learning fast!'
+//Solution
+console.log(greeting.flatMap(x => x.join(' ')).join(' '))
+
+//#4 Turn the trapped 3 number into: [3]
+const trapped = [[[[[[[[[[[[[[[[[[[[[[[[[[3]]]]]]]]]]]]]]]]]]]]]]]]]];
+//Solution
+console.log(trapped.flat(Infinity))
+// Infintiy is actually a LARGE number in JavaScipt. It represents the maximum amount of memory that we can hold for a number! Learn more here: https://riptutorial.com/javascript/example/2337/infinity-and--infinity
+
+//#5 Clean up this email to have no whitespaces. Make the answer be in a single line (return a new string):
+const userEmail3 = '     cannotfillemailformcorrectly@gmail.com   '
+//Solution:
+console.log(userEmail3.trimEnd().trimStart())
+
+
+//#6 Turn the below users (value is their ID number) into an array: [ [ 'user1', 18273 ], [ 'user2', 92833 ], [ 'user3', 90315 ] ]
+const users = { user1: 18273, user2: 92833, user3: 90315 }
+//Solution
+const usersArray = Object.entries(users)
+
+//#7 change the output array of the above to have the user's IDs multiplied by 2 -- Should output:[ [ 'user1', 36546 ], [ 'user2', 185666 ], [ 'user3', 180630 ] ]
+//Solution
+updatedUsersArray = usersArray.map((user) => [user[0], user[1] * 2])
+
+//#8 change the output array of question #7 back into an object with all the users IDs updated to their new version. Should output: { user1: 36546, user2: 185666, user3: 180630 }
+//Solution
+const updatedUsers = Object.fromEntries(updatedUsersArray)
+console.log(updatedUsers) */
+
+
+const basket = ['apples', 'oranges', 'grapes'];
+const detailedBasket = {
+  apples: 5,
+  oranges: 10,
+  grapes: 1000
+}
+
+//1
+for (let i = 0; i < basket.length; i++) {
+  console.log(basket[i]);
+}
+
+//2
+basket.forEach(item => {
+  console.log(item);
+})
+
+for (item in detailedBasket) {
+  console.log(item);
+}
+
+for (item of basket) {
+  console.log(item);
+}
+
+// Question #1:
+// create a function called biggestNumberInArray() that takes
+// an array as a parameter and returns the biggest number.
+// biggestNumberInArray([-1,0,3,100, 99, 2, 99]) should return 100;
+// Use at least 3 different types of javascript loops to write this:
+const array = [-1,0,3,100, 99, 2, 99] // should return 100
+const array2 = ['a', 3, 4, 2] // should return 4
+const array3 = [] // should return 0
+
+function biggestNumberInArray(arr) {
+  let highest = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (highest < arr[i]) {
+      highest = arr[i];
+    }
+  }
+  return highest
+}
+
+function biggestNumberInArray2(arr) {
+  let highest = 0;
+  arr.forEach(item => {
+    if (highest < item) {
+      highest = item;
+    }
+  })
+  return highest;
+}
+
+function biggestNumberInArray3(arr) {
+  let highest = 0;
+  for (item of arr) {
+    if (highest < item) {
+      highest = item;
+    }
+  }
+  return highest;
+}
+
+
+biggestNumberInArray3(array3)
+
+
+// Question #2:
+// Write a function checkBasket() that lets you know if the item is in the basket or not
+amazonBasket = {
+  glasses: 1,
+  books: 2,
+  floss: 100
+}
+
+function checkBasket(basket, lookingFor) {
+  for (item in basket) {
+    if (item === lookingFor) {
+      return `${lookingFor} is in your basket`
+    }
+  }
+  return 'that does not exist in your basket'
+}
