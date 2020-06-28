21f594bdd25f0dcfe86644ac1de6e1834f995e1c
jmbelmonte <jmbelmonte11@gmail.com>
2020-06-28 15:37:46 +0800

    Upd

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-var username = prompt("Username?");
-var password = prompt("Password?");
+const username = prompt("Username?");
+const password = prompt("Password?");
 
-if (username === "jm" && password === "123") {
+let greet;
+
+// var checkUsername = username === "jm" ? "True" : "False";
+
+const isUserValid = (username, password) => {
+    if (username === "jm" && password === "123") {
+        return `Welcome! Username: ${username} and Password: ${password}!`;
+    } else {
+        return `Wrong username and password!`;
+    }
+};
+
+console.log(isUserValid(username, password));
+
+/* if (username === "jm" && password === "123") {
     alert("Welcome to the system!");
 } else {
     alert("Wrong username and password!");
-}
+} */
+
